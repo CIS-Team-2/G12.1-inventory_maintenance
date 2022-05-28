75a9fc615f08d93b365dc4bed1a8338b32df7c7a
tepper-d <104649969+tepper-d@users.noreply.github.com>
2022-05-28 11:32:49 -0700

    Add project files.

---
+++ InventoryMaintenance/frmInvMaint.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/***************************************************************************************************
+* CIS 123: Introduction to OOP     |                                                               *
+* Murach's C#, 7th Edition         |    Exercise 12-1    Create and use a class                    *
+* Chapter 12 Extra Exercises       |    Add a class to an Inventory Maintenance application and    *
+* Team 2 Assignment, 28MAY2022     |    then add code to the two forms that use this class.        *
+* Patrick McKee & Dominique Tepper |                                                               *
+***************************************************************************************************/
+
+namespace InventoryMaintenance
+{
+    public partial class frmInvMaint : Form
+    {
+        public frmInvMaint()
+        {
+            InitializeComponent();
+        }
+
+        // Add a statement here that declares the list of items.
+
+
+        private void frmInvMaint_Load(object sender, EventArgs e)
+        {
+            // Add a statement here that gets the list of items.
+            FillItemListBox();
+        }
+
+        private void FillItemListBox()
+        {
+            lstItems.Items.Clear();
+            // Add code here that loads the list box with the items in the list.
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Add code here that creates an instance of the New Item form
+            // and then gets a new item from that form.
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int i = lstItems.SelectedIndex;
+            if (i != -1)
+            {
+                // Add code here that displays a dialog box to confirm
+                // the deletion and then removes the item from the list,
+                // saves the list of products, and refreshes the list box
+                // if the deletion is confirmed.
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ InventoryMaintenance/frmNewItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+
+/***************************************************************************************************
+* CIS 123: Introduction to OOP     |                                                               *
+* Murach's C#, 7th Edition         |    Exercise 12-1    Create and use a class                    *
+* Chapter 12 Extra Exercises       |    Add a class to an Inventory Maintenance application and    *
+* Team 2 Assignment, 28MAY2022     |    then add code to the two forms that use this class.        *
+* Patrick McKee & Dominique Tepper |                                                               *
+***************************************************************************************************/
+
+namespace InventoryMaintenance
+{
+    public partial class frmNewItem : Form
+    {
+        public frmNewItem()
+        {
+            InitializeComponent();
+        }
+
+        // Add a statement here that declares the inventory item.
+
+        // Add a method here that gets and returns a new item.
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+                // Add code here that creates a new item
+                // and closes the form.
+            }
+        }
+
+        private bool IsValidData()
+        {
+            return Validator.IsPresent(txtItemNo) &&
+                   Validator.IsInt32(txtItemNo) &&
+                   Validator.IsPresent(txtDescription) &&
+                   Validator.IsPresent(txtPrice) &&
+                   Validator.IsDecimal(txtPrice);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
